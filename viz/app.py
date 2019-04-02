73e007b2d0005a2b7af70c6eec3a64494e659f99
shortpoet <soriano.carlitos@gmail.com>
2019-04-01 22:30:27 -0500

    fixed config var

--- viz/app.py
+++ viz/app.py
@@ -10,9 +10,10 @@ app = Flask(__name__)
 app.debug = True
 
 # Use flask_pymongo to set up mongo connection
+app.config['MONGO_URI'] = os.environ['MONGODB_CONFIG']
 # app.config['MONGO_URI'] = os.environ['MONGODB_URI'] or "mongodb://localhost:27017/cocktail_db"
 # app.config['MONGO_URI'] = "mongodb://localhost:27017/cocktail_db"
-app.config['MONGO_URI'] = config
+# app.config['MONGO_URI'] = config
 mongo = PyMongo(app)
 
 # Route to render index.html template using data from Mongo
